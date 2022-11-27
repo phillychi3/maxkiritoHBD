079b9fe99d55ce4705458d7c6921ee761123f030
philly <phillychi3@gmail.com>
2022-11-28 03:16:08 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>
+    <h1>阿雷生日快樂</h1>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
