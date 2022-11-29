c9f8ef36d0809c59ab9cae5ca7a2e662ed31d6fb
philly <phillychi3@gmail.com>
2022-11-30 01:10:34 +0800

    goup

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,28 @@
 </script>
 
 <template>
-  <div>
-    <h1>阿雷生日快樂</h1>
+  <div class="main">
+    <h1>阿雷電神生日快樂</h1>
+    <span>twitter:</span><a href="https://twitter.com/maxkirito_yxiao">https://twitter.com/maxkirito_yxiao</a>
+    
+
 
   </div>
 </template>
 
 <style scoped>
+
+
+
+div.main{
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+
+}
+a{
+  color: #fff;
+  text-decoration: none;
+}
+
 </style>
